a13045ce788b43a24011071b12c2d27d76b3b6c1
peck-peck <72839902+peck-peck@users.noreply.github.com>
2021-11-05 20:26:07 +0000

    added media

--- nova pasta/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from notifications.models import *
-# Register your models here.
-
-admin.site.register(Notification)
--- nova pasta/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from notifications.views import notifications,delete_notifications
-
-
-urlpatterns=[
-    path('notifications/', notifications, name='show-notifications'),
-    path('delete_notifications/<int:notification_id>/',delete_notifications, name='delete_notifications')
-]
